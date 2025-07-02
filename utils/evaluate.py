4989ef0bd00429cf815944e9fcdd9a7666dde1dc
hossein khalilian <hse.khalilian08@gmail.com>
2025-07-02 14:12:23 +0000

    add some model inference notebook

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,14 +1,17 @@
-from jiwer import wer, cer
+from jiwer import cer, wer
+
+from utils.normalizer import persian_normalizer
 
 
-def evaluate_asr(predictions, references):
+def evaluate_asr(reference: list[str], hypothesis: list[str]) -> dict:
     """
-    Compute WER and CER using the jiwer library.
+    Compute WER and CER using the jiwer library after applying Persian normalization.
     """
-    pred_text = "\n".join(predictions)
-    ref_text = "\n".join(references)
 
-    wer_score = wer(ref_text, pred_text)
-    cer_score = cer(ref_text, pred_text)
+    reference_normalized = [persian_normalizer(r) for r in reference]
+    hypothesis_normalized = [persian_normalizer(h) for h in hypothesis]
+
+    wer_score = wer(reference_normalized, hypothesis_normalized)
+    cer_score = cer(reference_normalized, hypothesis_normalized)
 
     return {"wer": wer_score, "cer": cer_score}
