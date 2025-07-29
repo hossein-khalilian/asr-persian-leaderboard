08fec979e4cc7ccd8ed6cfe9a8f9a5a5975e8ea7
hossein khalilian <hse.khalilian08@gmail.com>
2025-07-29 16:56:30 +0000

    update everything

--- run_models.sh
+++ run_models.sh
@@ -16,6 +16,37 @@ models=(
   # "torchaudio:torchaudio/wav2vec2-large"
 )
 
+
+models=(
+    "wav2vec2:alifarokh/wav2vec2-xls-r-300m-fa"
+    "wav2vec2:facebook/mms-1b-all"
+    "wav2vec2:hsekhalilian/wav2vec2-custom-model"
+    "wav2vec2:jonatasgrosman/wav2vec2-large-xlsr-53-persian"
+    "wav2vec2:m3hrdadfi/wav2vec2-large-xlsr-persian-shemo"
+    "wav2vec2:m3hrdadfi/wav2vec2-large-xlsr-persian-v2"
+    "wav2vec2:m3hrdadfi/wav2vec2-large-xlsr-persian-v3"
+    "nemo:nvidia/stt_fa_fastconformer_hybrid_large"
+    "nemo:alifarokh/nemo-conformer-medium-fa"
+    "seamless:facebook/hf-seamless-m4t-medium"
+    "seamless:facebook/seamless-m4t-large"
+    "seamless:facebook/seamless-m4t-medium"
+    "seamless:facebook/seamless-m4t-v2-large"
+    "whisper:MohammadGholizadeh/whisper-large-v3-persian-common-voice-17"
+    "whisper:MohammadKhosravi/whisper-large-v3-Persian"
+    "whisper:Neurai/NeuraSpeech_900h"
+    "whisper:Neurai/NeuraSpeech_WhisperBase"
+    "whisper:openai/whisper-base"
+    "whisper:openai/whisper-large"
+    "whisper:openai/whisper-large-v2"
+    "whisper:openai/whisper-large-v3"
+    "whisper:openai/whisper-large-v3-turbo"
+    "whisper:openai/whisper-medium"
+    "whisper:openai/whisper-small"
+    "whisper:steja/whisper-large-persian"
+    "whisper:distil-whisper/distil-large-v3"
+    "whisper:distil-whisper/distil-large-v3.5"
+)
+
 for entry in "${models[@]}"; do
   IFS=":" read -r model_type model_name <<< "$entry"
   echo "Running: make submit model_type=$model_type model_name=$model_name"
